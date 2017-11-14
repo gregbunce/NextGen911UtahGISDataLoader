084e861660d385f58af6e4baa02db282a82838de
gregbunce <bunce.greg@gmail.com>
2017-11-14 16:10:27 -0700

    get points that offset a polyline on right and left side

---
+++ NextGen911DataLoader/commands/GetRightLeftOffsetPointsFromPolyline.cs
@@ -0,0 +1,42 @@
+﻿using ArcGIS.Core.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGen911DataLoader.commands
+{
+    class GetRightLeftOffsetPointsFromPolyline
+    {
+
+        public static List<MapPoint> Execute(Polyline polyline)
+        {
+            try
+            {
+                MapPoint pt_right = GeometryEngine.Instance.MovePointAlongLine(polyline, 0.5, true, 55, SegmentExtension.NoExtension);
+                MapPoint pt_left = GeometryEngine.Instance.MovePointAlongLine(polyline, 0.5, true, -55, SegmentExtension.NoExtension);
+
+                List<MapPoint> mapPoints = new List<MapPoint>;
+                mapPoints.Add(pt_left);
+                mapPoints.Add(pt_right);
+
+                return mapPoints;
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was an error with GetRightLeftOffsetPointsFromPolyline method." +
+                ex.Message + " " + ex.Source + " " + ex.InnerException + " " + ex.HResult + " " + ex.StackTrace + " " + ex);
+
+                return null;
+            }
+        }
+
+
+
+
+
+    }
+}
+ 
