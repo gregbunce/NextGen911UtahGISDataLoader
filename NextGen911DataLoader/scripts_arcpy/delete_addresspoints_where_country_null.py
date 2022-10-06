0abec62c1ae2ce0edfa903699af6cbdd72cb4d4c
Greg Bunce <gbunce@utah.gov>
2022-10-06 17:34:56 -0600

    add python scripts

---
+++ NextGen911DataLoader/scripts_arcpy/delete_addresspoints_where_country_null.py
@@ -0,0 +1,10 @@
+import arcpy, sys, os
+
+query_filter = "Country is null"
+int_counter = 0
+
+with arcpy.da.UpdateCursor(r"C:\Temp\NG911GIS_Schema.gdb\AddressPoints", "*", where_clause=query_filter) as uCur:
+    for dRow in uCur:
+        int_counter = int_counter + 1
+        print("deleted " + str(int_counter))
+        uCur.deleteRow ()
